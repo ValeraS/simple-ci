a276163d2bbd1bffc8a7c1630d2d32e1c8db861a
ValeraS <ValeraS@users.noreply.github.com>
2019-10-19 04:31:16 +0300

    fix: add check of param

--- server/index.js
+++ server/index.js
@@ -3,4 +3,9 @@ const startServer = require('./src/server');
 
 console.table(config);
 
+if (!config.repo) {
+  console.log('Repo should be provided.');
+  process.exit(1);
+}
+
 startServer(config);
